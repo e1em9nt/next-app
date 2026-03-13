a80e49c1b1d747a9028978fd733599555223baa4
e1em9nt <nastusiyka@gmail.com>
2026-03-13 23:18:52 +0200

    feat: configure tanstack react-query provider
    query-provider.component.tsx: add QueryClientProvider wrapper
    shared/ui/query-provider/index.ts: export query provider
    shared/ui/index.ts: export query provider
    layout.tsx: wrap app with QueryProvider

--- src/app/(web)/[locale]/layout.tsx
+++ src/app/(web)/[locale]/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { QueryProvider } from "@/app/shared/ui";
 import "@/config/styles/globals.css";
 
 const geistSans = Geist({
@@ -28,7 +30,9 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
