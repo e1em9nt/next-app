538307280e11d1b7632f07aaf1649b6461d36b5f
e1em9nt <nastusiyka@gmail.com>
2026-03-15 23:55:00 +0200

    style: format style with eslint

--- src/app/modules/home-page/home-page.module.tsx
+++ src/app/modules/home-page/home-page.module.tsx
@@ -8,19 +8,27 @@ export default function HomePage() {
   const translations = useTranslations('HomePage');
 
   return (
-    <main className='relative h-[calc(100vh-64px)] overflow-hidden px-5'>
-        <Image src={bgImage} alt="Minimalistic Background" fill placeholder='blur' className='-z-10 object-cover' />
-        <div className="font-sans flex flex-col items-center justify-center gap-10 h-full">
-          <div className='flex flex-col items-center justify-center gap-5'>
-          <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold text-gray-700'>
+    <main className="relative h-[calc(100vh-64px)] overflow-hidden px-5">
+      <Image
+        src={bgImage}
+        alt="Minimalistic Background"
+        fill
+        placeholder="blur"
+        className="-z-10 object-cover"
+      />
+      <div className="font-sans flex flex-col items-center justify-center gap-10 h-full">
+        <div className="flex flex-col items-center justify-center gap-5">
+          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-700">
             {translations('heading')}
           </h1>
-          <p className='text-sm sm:text-base text-gray-700 sm:w-1/2 text-center'> 
-            {translations('subheading')} 
+          <p className="text-sm sm:text-base text-gray-700 sm:w-1/2 text-center">
+            {translations('subheading')}
           </p>
-          </div>
-          <Link href='#' className={buttonVariants({ variant: 'default' })}>{translations('actions.viewProducts')}</Link>
         </div>
+        <Link href="#" className={buttonVariants({ variant: 'default' })}>
+          {translations('actions.viewProducts')}
+        </Link>
+      </div>
     </main>
   );
 }
