652eba4633c911bdb52028b39cf459b638f34847
e1em9nt <nastusiyka@gmail.com>
2026-03-19 12:16:22 +0200

    refactor: add main tag for better accessibility and SEO

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -6,7 +6,9 @@ export default function GlobalError() {
   return (
     <html lang="en">
       <body>
-        <ErrorFallback />
+        <main>
+          <ErrorFallback />
+        </main>
       </body>
     </html>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,16 +11,18 @@ export default function NotFound() {
   return (
     <html lang="en">
       <body>
-        <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center px-4 py-8 text-center">
-          <h2 className="mb-6 text-5xl font-semibold">Whoops!</h2>
-          <h3 className="mb-1.5 text-3xl font-semibold">Something went wrong</h3>
-          <p className="text-muted-foreground mb-6 max-w-sm">
-            The page you&apos;re looking for isn&apos;t found, we suggest you back to home.
-          </p>
-          <Link href="/" className={buttonVariants({ variant: 'default' })}>
-            Back to home
-          </Link>
-        </div>
+        <main>
+          <div className="min-h-[calc(100vh-64px)] flex flex-col items-center justify-center px-4 py-8 text-center">
+            <h2 className="mb-6 text-5xl font-semibold">Whoops!</h2>
+            <h3 className="mb-1.5 text-3xl font-semibold">Something went wrong</h3>
+            <p className="text-muted-foreground mb-6 max-w-sm">
+              The page you&apos;re looking for isn&apos;t found, we suggest you back to home.
+            </p>
+            <Link href="/" className={buttonVariants({ variant: 'default' })}>
+              Back to home
+            </Link>
+          </div>
+        </main>
       </body>
     </html>
   );
