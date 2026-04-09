32c8bf5dc667035452e1c546e02d5d3012c41869
e1em9nt <nastusiyka@gmail.com>
2026-04-09 13:51:52 +0300

    refactor: replace SpinnerComponent with new Spinner implementation
    loading.tsx: update to use the new shadcn Spinner
    spinner.tsx: add new shadcn spinner component

--- src/app/modules/product/product.module.tsx
+++ src/app/modules/product/product.module.tsx
@@ -16,7 +16,7 @@ const ProductModule: FC<Readonly<IProps>> = (props) => {
 
   // return
   return (
-    <main className='mx-8 my-4 flex min-h-[calc(100vh-100px)] items-center justify-center sm:mx-10 md:mx-14'>
+    <main className='mx-8 my-4 flex min-h-[calc(100vh-64px)] items-center justify-center sm:mx-10 md:mx-14'>
       <div className='w-full lg:w-1/2 2xl:w-1/3'>
         <ProductCardComponent product={product} variant='detailed' />
       </div>
